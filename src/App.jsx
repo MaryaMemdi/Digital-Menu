5e186ff50d0a633cce8d8d6a94a2b7307230c5cf
MaryaMemdi <Maryamemadi78@gmail.com>
2024-07-11 17:37:34 +0330

    first commit and make a digital menu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/header";
+import Menu from "./components/menu";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/menu.jsx
@@ -0,0 +1,14 @@
+import { pizzaData } from "../../public/data";
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h2>our menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+    </div>
+  );
+};
+
+export default Menu;
